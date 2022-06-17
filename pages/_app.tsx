231ada22ad6e52b3a70af0c2759ba9d8b4d546d4
Scarlate <scarlate100@gmail.com>
2022-06-17 18:25:11 -0300

    initial commit settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
+import React from 'react'
+import type { ReactElement, ReactNode } from 'react'
 import type { AppProps } from 'next/app'
+import type { NextPage } from 'next'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ThemeProvider } from 'styled-components'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { AuthProvider } from '../src/hooks/context/auth-context'
+
+import { GlobalStyle, theme } from '../styles/global-style'
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const queryClient = new QueryClient()
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
